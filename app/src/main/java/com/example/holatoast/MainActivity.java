209674f8ca7869dfb8f1a9f15ca20fb4489f8da0
Dani Pulido <dpvdevelopers@gmail.com>
2020-10-07 16:21:33 +0200

    Contador terminado con constraint layout

---
+++ app/src/main/java/com/example/holatoast/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.holatoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int contador;
+    private TextView principal;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        contador = 0;
+        principal = findViewById(R.id.textViewContador);
+    }
+
+    public void saludar(View view) {
+
+    }
+
+    public void sumar(View view) {
+        contador ++;
+        if(contador==1000) principal.setTextSize(150);
+        principal.setText(String.valueOf(contador));
+    }
+}
